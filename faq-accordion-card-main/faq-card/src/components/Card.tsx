02a47563f2c79f57b20a13ae77bbb083a4ffba66
LeandroF01 <leaylio3327@gmail.com>
2023-02-06 15:19:17 -0300

    Refactor render conditional and list of database

--- faq-accordion-card-main/faq-card/src/components/Card.tsx
+++ faq-accordion-card-main/faq-card/src/components/Card.tsx
@@ -1,8 +1,24 @@
 import box from "../../../images/illustration-box-desktop.svg";
 import avatar from "../../../images/illustration-woman-online-desktop.svg";
 import arrow from "../../../images/icon-arrow-down.svg";
+import React, { useState } from "react";
+import data from "../json/database.json";
 
 const Card = () => {
+  const [value, setValue] = useState<Number>(0);
+
+  const handleCLick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    const filter = data.filter((el) => {
+      el.id === Number(e.target.dataset.number) && setValue(el.id);
+    });
+
+    if (value !== 0) {
+      setValue(0);
+    }
+
+    return filter;
+  };
+
   return (
     <article className="card">
       <section className="container-image">
@@ -11,56 +27,29 @@ const Card = () => {
       <img className="card-box" src={box} alt="" />
       <section className="container-questions">
         <h2 className="card-title">FAQ</h2>
-        <article className="card-questions">
-          How many team members can I invite?
-          <article className="text-questions">
-            You can invite up to 2 additional users on the Free plan. There is
-            no limit on team members for the Premium plan.
+
+        {data.map(({ id, questions, text }) => (
+          <article
+            key={id}
+            className={`card-questions ${value === id ? "color-text" : ""}`}>
+            {questions}
+            {value === id ? (
+              <article className="text-questions">{text}</article>
+            ) : null}
+            <button
+              onClick={handleCLick}
+              name="buttonQuest"
+              data-number={id}
+              className="arrow-questions">
+              <img
+                data-number={id}
+                className={value === id ? "arrow-up" : ""}
+                src={arrow}
+                alt="arrow"
+              />
+            </button>
           </article>
-          <button className="arrow-questions">
-            <img src={arrow} alt="arrow" />
-          </button>
-        </article>
-        <article className="card-questions">
-          What is the maximum file upload size?
-          <article className="text-questions">
-            No more than 2GB. All files in your account must fit your allotted
-            storage space.
-          </article>
-          <button className="arrow-questions">
-            <img src={arrow} alt="arrow" />
-          </button>
-        </article>
-        <article className="card-questions">
-          How do I reset my password?
-          <article className="text-questions">
-            Click “Forgot password” from the login page or “Change password”
-            from your profile page. A reset link will be emailed to you.
-          </article>
-          <button className="arrow-questions">
-            <img src={arrow} alt="arrow" />
-          </button>
-        </article>
-        <article className="card-questions">
-          Can I cancel my subscription?
-          <article className="text-questions">
-            Yes! Send us a message and we’ll process your request no questions
-            asked.
-          </article>
-          <button className="arrow-questions">
-            <img src={arrow} alt="arrow" />
-          </button>
-        </article>
-        <article className="card-questions">
-          Do you provide additional support?
-          <article className="text-questions">
-            Chat and email support is available 24/7. Phone lines are open
-            during normal business hours.
-          </article>
-          <button className="arrow-questions">
-            <img src={arrow} alt="arrow" />
-          </button>
-        </article>
+        ))}
       </section>
     </article>
   );
