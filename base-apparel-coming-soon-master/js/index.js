2e254538b2cfd2c26df48562f89e6b604fc09f71
LeandroF01 <leaylio3327@gmail.com>
2023-04-14 17:17:34 -0300

    Add logic validation form

--- base-apparel-coming-soon-master/js/index.js
+++ base-apparel-coming-soon-master/js/index.js
@@ -1,4 +1,16 @@
-const form = document.querySelector("");
-const input = document.querySelector("");
+const form = document.querySelector(".form");
+const inputForm = document.querySelector(".form__input");
+const validation = document.querySelector(".input__icon");
 
-form.addEventListener("");
+form.addEventListener("submit", (event) => {
+	event.preventDefault();
+	const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+	const validate = emailRegex.test(inputForm.value);
+
+	if (validate) {
+		inputForm.value = "";
+		validation.classList.remove("active");
+	} else {
+		validation.classList.add("active");
+	}
+});
