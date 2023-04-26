2d919af24bd1de93c408d6b2b2f76308e12a618e
LeandroF01 <leaylio3327@gmail.com>
2023-04-26 16:02:10 -0300

    Add db notify

---
+++ junior/notifications-page-main/notify_page/src/ts/db.ts
@@ -0,0 +1,44 @@
+const obj = [
+	{
+		name = "Mark Webber",
+		type = "reacted to your recent post ",
+		post = "My first tournament today!",
+		time = "1m ago",
+	},
+	{
+		name = "Angela Gray",
+		type = "followed you",
+		time = "5m ago",
+	},
+	{
+		name = "Jacob Thompson",
+		type = "has joined your group",
+		group = "Chess Club",
+		time = "1 day ago",
+	},
+	{
+		name = "Rizky Hasanuddin",
+		type = "sent you a private message",
+		time = "5m ago",
+		message = `Hello, thanks for setting up the Chess Club. I've been a member for a few weeks now and
+  I'm already having lots of fun and improving my game.`,
+	},
+	{
+		name = "Kimberly Smith",
+		type = "commented on your picture",
+		time = "1 week ago",
+		image = "./",
+	},
+	{
+		name = "Nathan Peterson",
+		type = "reacted to your recent post ",
+		post = "5 end-game strategies to increase your win rate",
+		time = "2 weeks ago",
+	},
+	{
+		name = "Anna Kim",
+		type = "left the group ",
+		group = "Chess Club",
+		time = "2 weeks ago",
+	},
+];
