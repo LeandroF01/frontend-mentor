f14cec1dcbac6aaaba9ff4f9082f043443ded34c
LeandroF01 <leaylio3327@gmail.com>
2023-04-27 18:50:25 -0300

    Add info list of obj, persons notify

--- junior/notifications-page-main/notify_page/src/ts/db.ts
+++ junior/notifications-page-main/notify_page/src/ts/db.ts
@@ -1,44 +1,75 @@
+import angela from "../../public/assets/images/avatar-angela-gray.webp";
+import anna from "../../public/assets/images/avatar-anna-kim.webp";
+import jacob from "../../public/assets/images/avatar-jacob-thompson.webp";
+import kimberly from "../../public/assets/images/avatar-kimberly-smith.webp";
+import mark from "../../public/assets/images/avatar-mark-webber.webp";
+import nathan from "../../public/assets/images/avatar-nathan-peterson.webp";
+import rizky from "../../public/assets/images/avatar-rizky-hasanuddin.webp";
+
 const obj = [
 	{
-		name = "Mark Webber",
-		type = "reacted to your recent post ",
-		post = "My first tournament today!",
-		time = "1m ago",
+		id: 1,
+		name: "Mark Webber",
+		avatar: mark,
+		type: "reacted to your recent post ",
+		post: "My first tournament today!",
+		time: "1m ago",
+		status: "unread",
 	},
 	{
-		name = "Angela Gray",
-		type = "followed you",
-		time = "5m ago",
+		id: 2,
+		name: "Angela Gray",
+		avatar: angela,
+		type: "followed you",
+		time: "5m ago",
+		status: "unread",
 	},
 	{
-		name = "Jacob Thompson",
-		type = "has joined your group",
-		group = "Chess Club",
-		time = "1 day ago",
+		id: 3,
+		name: "Jacob Thompson",
+		avatar: jacob,
+		type: "has joined your group",
+		group: "Chess Club",
+		time: "1 day ago",
+		status: "unread",
 	},
 	{
-		name = "Rizky Hasanuddin",
-		type = "sent you a private message",
-		time = "5m ago",
-		message = `Hello, thanks for setting up the Chess Club. I've been a member for a few weeks now and
+		id: 4,
+		name: "Rizky Hasanuddin",
+		avatar: rizky,
+		type: "sent you a private message",
+		time: "5m ago",
+		status: "read",
+		message: `Hello, thanks for setting up the Chess Club. I've been a member for a few weeks now and
   I'm already having lots of fun and improving my game.`,
 	},
 	{
-		name = "Kimberly Smith",
-		type = "commented on your picture",
-		time = "1 week ago",
-		image = "./",
+		id: 5,
+		name: "Kimberly Smith",
+		avatar: kimberly,
+		type: "commented on your picture",
+		time: "1 week ago",
+		status: "read",
+		image: "./",
 	},
 	{
-		name = "Nathan Peterson",
-		type = "reacted to your recent post ",
-		post = "5 end-game strategies to increase your win rate",
-		time = "2 weeks ago",
+		id: 6,
+		name: "Nathan Peterson",
+		avatar: nathan,
+		type: "reacted to your recent post ",
+		post: "5 end-game strategies to increase your win rate",
+		time: "2 weeks ago",
+		status: "read",
 	},
 	{
-		name = "Anna Kim",
-		type = "left the group ",
-		group = "Chess Club",
-		time = "2 weeks ago",
+		id: 7,
+		name: "Anna Kim",
+		avatar: anna,
+		type: "left the group ",
+		group: "Chess Club",
+		time: "2 weeks ago",
+		status: "read",
 	},
 ];
+
+export default obj;
