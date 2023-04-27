c14a25a5db3f4694c8d0a18e55215aa21ac77da8
LeandroF01 <leaylio3327@gmail.com>
2023-04-27 18:51:36 -0300

    Refactor page to new component

--- junior/notifications-page-main/notify_page/src/app/page.tsx
+++ junior/notifications-page-main/notify_page/src/app/page.tsx
@@ -1,13 +1,11 @@
 import Image from "next/image";
+import { Modal } from "../components/Modal";
+import "./style.css";
 
 export default function Home() {
 	return (
-		<section className="container__notify">
-			<header className="notify">
-				<h2 className="notify__title">Notifications 3</h2>
-				<span className="notify__num">3</span>
-				<button className="notify__read">Mark all as read</button>
-			</header>
-		</section>
+		<main className="container__modal">
+			<Modal />
+		</main>
 	);
 }
